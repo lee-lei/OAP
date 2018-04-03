c6793bbaf94ce80a96fb50c462ab122a12515590
Lei Li <lei.l.li@intel.com>
2018-04-03 13:57:19 +0800

    Refactoring bitmap index scanner
    1. Refactoring BitMapScanner and abstract the common BitmapReader
    2. Add BitmapReaderV1 based on the original BitMap Scanner for backward
       compatibility
    3. Add BitmapReaderV2 to directly get row Id list from fiber caches

---
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BitmapReader.scala
@@ -0,0 +1,210 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.datasources.oap.index
+
+import scala.collection.mutable.ArrayBuffer
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FSDataInputStream, Path}
+
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.codegen.GenerateOrdering
+import org.apache.spark.sql.execution.datasources.OapException
+import org.apache.spark.sql.execution.datasources.oap.Key
+import org.apache.spark.sql.execution.datasources.oap.filecache.{BitmapFiber, FiberCache, FiberCacheManager, MemoryManager, WrappedFiberCache}
+import org.apache.spark.sql.execution.datasources.oap.io.IndexFile
+import org.apache.spark.sql.execution.datasources.oap.statistics.{StatisticsManager, StatsAnalysisResult}
+import org.apache.spark.sql.execution.datasources.oap.utils.NonNullKeyReader
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.util.ShutdownHookManager
+
+private[oap] case class BitmapReader(
+    intervalArray: ArrayBuffer[RangeInterval],
+    keySchema: StructType,
+    idxPath: Path,
+    conf: Configuration) {
+
+  private var _totalRows: Long = 0
+  // TODO: use hash instead of order compare.
+  @transient protected var ordering: Ordering[Key] =
+    GenerateOrdering.create(keySchema)
+  @transient
+  protected lazy val nnkr: NonNullKeyReader = new NonNullKeyReader(keySchema)
+
+  protected val BITMAP_FOOTER_SIZE = 6 * 8
+
+  protected var bmUniqueKeyListCount: Int = _
+
+  protected var bmUniqueKeyListCache: WrappedFiberCache = _
+  protected var bmOffsetListCache: WrappedFiberCache = _
+  protected var bmFooterCache: WrappedFiberCache = _
+  protected var bmNullEntryOffset: Int = _
+  protected var bmNullEntrySize: Int = _
+
+  override def toString: String = "BitmapReader"
+
+  private def getFooterCache(fin: FSDataInputStream): Unit = {
+    val idxFileSize = idxPath.getFileSystem(conf).getFileStatus(idxPath).getLen
+    val footerOffset = idxFileSize.toInt - BITMAP_FOOTER_SIZE
+    val footerFiber = BitmapFiber(
+      () => loadBmSection(fin, footerOffset, BITMAP_FOOTER_SIZE),
+      idxPath.toString, BitmapIndexSectionId.footerSection, 0)
+    bmFooterCache = WrappedFiberCache(FiberCacheManager.get(footerFiber, conf))
+    // Calculate total rows right after footer cache is loaded.
+    _totalRows = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE * 7)
+
+  }
+
+  private def checkVersionNum(versionNum: Int): Unit =
+    if (IndexFile.VERSION_NUM != versionNum) {
+      throw new OapException("Bitmap Index File version is not compatible!")
+    }
+
+  protected def loadBmSection(fin: FSDataInputStream, offset: Int, size: Int): FiberCache =
+    MemoryManager.toIndexFiberCache(fin, offset.toLong, size)
+
+  protected def getIdxOffset(fiberCache: FiberCache, baseOffset: Long, idx: Int): Int =
+    fiberCache.getInt(baseOffset + idx * 4)
+
+  protected def getKeyIdx(keySeq: Seq[InternalRow], range: RangeInterval): (Int, Int) = {
+    val keyLength = keySeq.length
+    val startIdx = if (range.start == IndexScanner.DUMMY_KEY_START) {
+      // If no starting key, assume to start from the first key.
+      0
+    } else {
+     // Find the first index to be > or >= range.start. If no found, return -1.
+      val (idx, found) =
+         IndexUtils.binarySearch(0, keyLength, keySeq(_), range.start, ordering.compare)
+      if (found) {
+        if (range.startInclude) idx else idx + 1
+      } else if (ordering.compare(keySeq.head, range.start) > 0) {
+        0
+      } else {
+        -1
+      }
+    }
+    // If invalid starting index, just return.
+    if (startIdx == -1 || startIdx == keyLength) {
+      return (-1, -1)
+    }
+    // If equal query, no need to find endIdx.
+    if (range.start == range.end && range.start != IndexScanner.DUMMY_KEY_START) {
+      return (startIdx, startIdx)
+    }
+
+    val endIdx = if (range.end == IndexScanner.DUMMY_KEY_END) {
+      // If no ending key, assume to end with the last key.
+      keyLength - 1
+    } else {
+      // The range may be invalid. I.e. endIdx may be little than startIdx.
+      // So find endIdx from the beginning.
+      val (idx, found) =
+         IndexUtils.binarySearch(0, keyLength, keySeq(_), range.end, ordering.compare)
+      if (found) {
+        if (range.endInclude) idx else idx - 1
+      } else if (ordering.compare(keySeq.last, range.end) < 0) {
+        keyLength - 1
+      } else {
+        -1
+      }
+    }
+    (startIdx, endIdx)
+  }
+
+  protected def readBmUniqueKeyList(data: FiberCache): Seq[InternalRow] = {
+    var curOffset = 0
+    (0 until bmUniqueKeyListCount).map( idx => {
+      val (value, length) =
+        nnkr.readKey(data, curOffset)
+      curOffset += length
+      value
+    })
+  }
+
+  protected def getDesiredSegments(fin: FSDataInputStream): Unit = {
+    getFooterCache(fin)
+    assert(bmFooterCache != null)
+    val versionNum = bmFooterCache.fc.getInt(0)
+    checkVersionNum(versionNum)
+    val uniqueKeyListTotalSize = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE)
+    bmUniqueKeyListCount = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE * 2)
+    val entryListTotalSize = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE * 3)
+    val offsetListTotalSize = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE * 4)
+    bmNullEntryOffset = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE * 5)
+    bmNullEntrySize = bmFooterCache.fc.getInt(IndexUtils.INT_SIZE * 6)
+
+    // Get the offset for the different segments in bitmap index file.
+    val uniqueKeyListOffset = IndexFile.VERSION_LENGTH
+    val entryListOffset = uniqueKeyListOffset + uniqueKeyListTotalSize
+    val offsetListOffset = entryListOffset + entryListTotalSize + bmNullEntrySize
+
+    val uniqueKeyListFiber = BitmapFiber(
+      () => loadBmSection(fin, uniqueKeyListOffset, uniqueKeyListTotalSize),
+      idxPath.toString, BitmapIndexSectionId.keyListSection, 0)
+    bmUniqueKeyListCache = WrappedFiberCache(FiberCacheManager.get(uniqueKeyListFiber, conf))
+
+    val offsetListFiber = BitmapFiber(
+      () => loadBmSection(fin, offsetListOffset, offsetListTotalSize),
+      idxPath.toString, BitmapIndexSectionId.entryOffsetsSection, 0)
+    bmOffsetListCache = WrappedFiberCache(FiberCacheManager.get(offsetListFiber, conf))
+  }
+
+  protected def clearCache(): Unit = {
+    if (bmUniqueKeyListCache != null) {
+      bmUniqueKeyListCache.release
+    }
+    if (bmOffsetListCache != null) {
+      bmOffsetListCache.release
+    }
+    if (bmFooterCache != null) {
+      bmFooterCache.release
+    }
+  }
+
+  def totalRows(): Long = _totalRows
+
+  def close(fin: FSDataInputStream): Unit =
+    try {
+      fin.close
+    } catch {
+      case e: Exception =>
+        if (!ShutdownHookManager.inShutdown()) {
+          throw new OapException("Exception in FSDataInputStream.close()", e)
+      }
+    }
+
+  def analyzeStatistics(): StatsAnalysisResult = {
+    val fin = idxPath.getFileSystem(conf).open(idxPath)
+    getFooterCache(fin)
+    // The stats offset and size are located in the end of bitmap footer segment.
+    // See the comments in BitmapIndexRecordWriter.scala.
+    val statsOffset = bmFooterCache.fc.getLong(BITMAP_FOOTER_SIZE - IndexUtils.LONG_SIZE * 2)
+    val statsSize = bmFooterCache.fc.getLong(BITMAP_FOOTER_SIZE - IndexUtils.LONG_SIZE)
+    val bmStatsContentFiber = BitmapFiber(
+      () => loadBmSection(fin, statsOffset.toInt, statsSize.toInt),
+      idxPath.toString, BitmapIndexSectionId.statsContentSection, 0)
+    val bmStatsContentCache = WrappedFiberCache(FiberCacheManager.get(bmStatsContentFiber, conf))
+    val stats = StatisticsManager.read(bmStatsContentCache.fc, 0, keySchema)
+    val res = StatisticsManager.analyse(stats, intervalArray, conf)
+    bmFooterCache.release
+    bmStatsContentCache.release
+    close(fin)
+    res
+  }
+
+}
---
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BitmapReaderV1.scala
@@ -0,0 +1,220 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.datasources.oap.index
+
+import java.io.DataInput
+import java.io.EOFException
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FSDataInputStream, Path}
+import org.roaringbitmap.FastAggregation
+import org.roaringbitmap.RoaringBitmap
+
+import org.apache.spark.sql.execution.datasources.oap.filecache.{BitmapFiber, FiberCache, FiberCacheManager, WrappedFiberCache}
+import org.apache.spark.sql.execution.datasources.oap.io.IndexFile
+import org.apache.spark.sql.types.StructType
+
+private[oap] class BitmapReaderV1(
+    fin: FSDataInputStream,
+    intervalArray: ArrayBuffer[RangeInterval],
+    internalLimit: Int,
+    keySchema: StructType,
+    idxPath: Path,
+    conf: Configuration)
+    extends BitmapReader(intervalArray, keySchema, idxPath, conf) with Iterator[Int] {
+
+  @transient private var bmRowIdIterator: Iterator[Integer] = _
+  private var empty: Boolean = _
+
+  override def hasNext: Boolean = !empty && bmRowIdIterator.hasNext
+  override def next(): Int = bmRowIdIterator.next()
+  override def toString: String = "BitmapReaderV1"
+
+  private def getDesiredBitmap(fc: FiberCache): RoaringBitmap = {
+    val stream = new BitmapDataInputStream(fc)
+    val entry = new RoaringBitmap()
+    // Below is directly reading from byte array rather than deserializing into java object.
+    entry.deserialize(stream)
+    entry
+  }
+
+  private def getDesiredBitmapArray(): ArrayBuffer[RoaringBitmap] = {
+    val keySeq = readBmUniqueKeyList(bmUniqueKeyListCache.fc)
+    intervalArray.flatMap{
+      case range if !range.isNullPredicate =>
+        val (startIdx, endIdx) = getKeyIdx(keySeq, range)
+        if (startIdx == -1 || endIdx == -1) {
+          // range not fond in cur bitmap, return empty for performance consideration
+          Seq.empty
+        } else {
+          (startIdx until (endIdx + 1)).map(idx => {
+            val curIdxOffset = getIdxOffset(bmOffsetListCache.fc, 0L, idx)
+            val entrySize = getIdxOffset(bmOffsetListCache.fc, 0L, idx + 1) - curIdxOffset
+            val entryFiber = BitmapFiber(() => loadBmSection(fin, curIdxOffset, entrySize),
+              idxPath.toString, BitmapIndexSectionId.entryListSection, idx)
+            val entryCache = WrappedFiberCache(FiberCacheManager.get(entryFiber, conf))
+            val entry = getDesiredBitmap(entryCache.fc)
+            entryCache.release
+            entry
+          })
+        }
+      case range if range.isNullPredicate =>
+        val nullListFiber = BitmapFiber(
+          () => loadBmSection(fin, bmNullEntryOffset, bmNullEntrySize),
+          idxPath.toString, BitmapIndexSectionId.entryNullSection, 0)
+        val nullListCache = WrappedFiberCache(FiberCacheManager.get(nullListFiber, conf))
+        if (nullListCache.fc.size != 0) {
+          val entry = getDesiredBitmap(nullListCache.fc)
+          nullListCache.release
+          Seq(entry)
+        } else {
+          Seq.empty
+        }
+    }
+  }
+
+  def getRowIdIterator(): Unit = {
+    try {
+      getDesiredSegments(fin)
+      val bitmapArray = getDesiredBitmapArray
+      if (bitmapArray.nonEmpty) {
+        if (internalLimit > 0) {
+          // Get N items from each index.
+          bmRowIdIterator =
+            bitmapArray.flatMap(bm => bm.iterator.asScala.take(internalLimit)).iterator
+        } else {
+          bmRowIdIterator =
+            bitmapArray.reduceLeft(FastAggregation.or(_, _)).iterator.asScala
+        }
+        empty = false
+      } else {
+        empty = true
+      }
+    } finally {
+      clearCache
+    }
+  }
+}
+
+// Below class is used to directly decode bitmap from FiberCache(either offheap/onheap memory).
+private[oap] class BitmapDataInputStream(bitsStream: FiberCache) extends DataInput {
+
+ private val bitsSize: Int = bitsStream.size.toInt
+ // The current position to read from FiberCache.
+ private var pos: Int = 0
+
+ // The reading byte order is big endian.
+ override def readShort(): Short = {
+   val curPos = pos
+   pos += 2
+   (((bitsStream.getByte(curPos) & 0xFF) << 8) |
+     ((bitsStream.getByte(curPos + 1) & 0xFF)) & 0xFFFF).toShort
+ }
+
+ override def readInt(): Int = {
+   val curPos = pos
+   pos += 4
+   ((bitsStream.getByte(curPos) & 0xFF) << 24) |
+     ((bitsStream.getByte(curPos + 1) & 0xFF) << 16) |
+     ((bitsStream.getByte(curPos + 2) & 0xFF) << 8) |
+     (bitsStream.getByte(curPos + 3) & 0xFF)
+ }
+
+ override def readLong(): Long = {
+   val curPos = pos
+   pos += 8
+   ((bitsStream.getByte(curPos) & 0xFF).toLong << 56) |
+     ((bitsStream.getByte(curPos + 1).toLong & 0xFF) << 48) |
+     ((bitsStream.getByte(curPos + 2).toLong & 0xFF) << 40) |
+     ((bitsStream.getByte(curPos + 3).toLong & 0xFF) << 32) |
+     ((bitsStream.getByte(curPos + 4).toLong & 0xFF) << 24) |
+     ((bitsStream.getByte(curPos + 5).toLong & 0xFF) << 16) |
+     ((bitsStream.getByte(curPos + 6).toLong & 0xFF) << 8) |
+     (bitsStream.getByte(curPos + 7).toLong & 0xFF)
+ }
+
+ override def readFully(readBuffer: Array[Byte], offset: Int, length: Int): Unit = {
+   if (length < 0) {
+     throw new IndexOutOfBoundsException("read length is inlegal for bitmap index.\n")
+   }
+   var curPos = pos
+   pos += length
+   if (pos > bitsSize - 1) {
+     throw new EOFException("read is ending of file for bitmap index.\n")
+   }
+   (offset until (offset + length)).foreach(idx => {
+     readBuffer(idx) = bitsStream.getByte(curPos)
+     curPos += 1
+   })
+ }
+
+ override def skipBytes(n: Int): Int = {
+   pos += n
+   n
+ }
+
+ // Below are not needed by roaring bitmap, just implement them for DataInput interface.
+ override def readBoolean(): Boolean = {
+   val curPos = pos
+   pos += 1
+   if (bitsStream.getByte(curPos).toInt != 0) true else false
+ }
+
+ override def readByte(): Byte = {
+   val curPos = pos
+   pos += 1
+   bitsStream.getByte(curPos)
+ }
+
+ override def readUnsignedByte(): Int = {
+   readByte().toInt
+ }
+
+ override def readUnsignedShort(): Int = {
+   readShort().toInt
+ }
+
+ override def readChar(): Char = {
+   readShort().toChar
+ }
+
+ override def readDouble(): Double = {
+   readLong().toDouble
+ }
+
+ override def readFloat(): Float = {
+   readInt().toFloat
+ }
+
+ override def readFully(readBuffer: Array[Byte]): Unit = {
+   readFully(readBuffer, 0, readBuffer.length)
+ }
+
+ override def readLine(): String = {
+   throw new UnsupportedOperationException("Bitmap doesn't need this." +
+     "It's inlegal to use it in bitmap!!!")
+ }
+
+ override def readUTF(): String = {
+   throw new UnsupportedOperationException("Bitmap doesn't need this." +
+     "It' inlegal to use it in bitmap!!!")
+ }
+}
