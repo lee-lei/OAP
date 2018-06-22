283fbab5f686049417f1ab4fc64f2144a4c24369
Lei Li <lei.l.li@intel.com>
2018-06-22 16:41:41 +0800

    Remove the specific unit test to test analyzeStatistics
    The other existing bitmap sql query test will also trigger the NPE as well.

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/index/BitmapAnalyzeStatisticsSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/index/BitmapAnalyzeStatisticsSuite.scala
@@ -17,28 +17,22 @@
 
 package org.apache.spark.sql.execution.datasources.oap.index
 
-import java.io.File
-
-import org.apache.hadoop.fs.{Path, RawLocalFileSystem}
+import org.apache.hadoop.fs.RawLocalFileSystem
 import org.scalatest.BeforeAndAfterEach
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{QueryTest, Row}
-import org.apache.spark.sql.execution.datasources.oap.{DataSourceMeta, OapFileFormat}
-import org.apache.spark.sql.execution.datasources.oap.statistics.StatsAnalysisResult
-import org.apache.spark.sql.internal.oap.OapConf
-import org.apache.spark.sql.sources._
 import org.apache.spark.sql.test.TestSparkSession
 import org.apache.spark.sql.test.oap.SharedOapContextBase
-import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 import org.apache.spark.util.Utils
 
 /*
  * By default, the spark and oap tests are using the same debug file system which
  * will re-open the file for each file read. Therefore the file read will not be
- * impacted even if the input stream is closed alrady.
+ * impacted even if the input stream is closed already.
  * This test is to mimic the same file system with the customer's QA environment.
- * Thus it will throw the exception if the input stream is closed before the file read.
+ * Thus it will throw the exception if the input stream is closed before the file read
+ * when the bimap scanner tries to analyze the statistics.
  */
 
 private[oap] class TestOapSessionWithRawLocalFileSystem(sc: SparkContext)
@@ -63,13 +57,9 @@ class BitmapAnalyzeStatisticsSuite extends QueryTest with SharedOapContextWithRa
     with BeforeAndAfterEach {
   import testImplicits._
 
-  private var tempDir: File = _
-  private var path: String = _
-
   override def beforeEach(): Unit = {
-    spark.conf.set(OapConf.OAP_ENABLE_EXECUTOR_INDEX_SELECTION.key, "true")
-    tempDir = Utils.createTempDir()
-    path = tempDir.getAbsolutePath
+    val tempDir = Utils.createTempDir()
+    val path = tempDir.getAbsolutePath
     sql(s"""CREATE TEMPORARY VIEW oap_test (a INT, b STRING)
             | USING oap
             | OPTIONS (path '$path')""".stripMargin)
@@ -77,37 +67,6 @@ class BitmapAnalyzeStatisticsSuite extends QueryTest with SharedOapContextWithRa
 
   override def afterEach(): Unit = {
     sqlContext.dropTempTable("oap_test")
-    spark.conf.set(OapConf.OAP_ENABLE_EXECUTOR_INDEX_SELECTION.key, "false")
-  }
-
-  test("Bitmap Scanner analyzes the statistics.") {
-    val data: Seq[(Int, String)] = (0 to 200).map {i => (i, s"this is test $i")}
-    data.toDF("key", "value").createOrReplaceTempView("t")
-    sql("insert overwrite table oap_test select * from t")
-    sql("create oindex idxa on oap_test(a) USING BITMAP")
-    val oapMetaPath = new Path(
-      new File(path, OapFileFormat.OAP_META_FILE).getAbsolutePath)
-    val oapMeta = DataSourceMeta.initialize(oapMetaPath, configuration)
-    val ic = new IndexContext(oapMeta)
-    val equalFilters: Array[Filter] = Array(Or(EqualTo("a", 14),
-      EqualTo("b", "this is test 166")))
-    val schema = StructType(StructField("a", IntegerType) :: Nil)
-    ScannerBuilder.build(equalFilters, ic)
-    assert(ic.getScanners.get.scanners.head.isInstanceOf[BitMapScanner])
-    val indexScanner = ic.getScanners.get.scanners.head
-    val bitmapScanner = BitMapScanner(indexScanner.meta).withKeySchema(schema)
-    bitmapScanner.intervalArray = indexScanner.intervalArray
-    val fileNameIterator = tempDir.listFiles()
-    for (fileName <- fileNameIterator) {
-      if (fileName.toString.endsWith(OapFileFormat.OAP_DATA_EXTENSION)) {
-        // Below will finally test the analyzeStatistics of BitMapScanner.
-        val bitmapRes = bitmapScanner.readBehavior(new Path(fileName.toString), configuration)
-        assert((bitmapRes == StatsAnalysisResult.FULL_SCAN ||
-          bitmapRes == StatsAnalysisResult.SKIP_INDEX ||
-          bitmapRes == StatsAnalysisResult.USE_INDEX), true)
-      }
-    }
-    sql("drop oindex idxa on oap_test")
   }
 
   test("Bitmap index typical equal test") {
